c812eced3338fd68271ff44c25f3c5eb5df11c52
Vladimir Grebnev <36560479+Grelodya@users.noreply.github.com>
2023-04-06 17:20:54 +0300

    Update solution.py

--- solution.py
+++ solution.py
@@ -4,12 +4,13 @@ import numpy as np
 from scipy.stats import norm
 
 
-chat_id = 123456 # Ваш chat ID, не меняйте название переменной
+chat_id = 382319199 # Ваш chat ID, не меняйте название переменной
 
 def solution(p: float, x: np.array) -> tuple:
     # Измените код этой функции
     # Это будет вашим решением
     # Не меняйте название функции и её аргументы
+    x = 2 * x / 32 ** 2
     alpha = 1 - p
     loc = x.mean()
     scale = np.sqrt(np.var(x)) / np.sqrt(len(x))
