1bc4b58d33e51dfa726d507d6fb6bbbcb8462451
Vladimir Grebnev <36560479+Grelodya@users.noreply.github.com>
2023-04-06 19:31:32 +0300

    Update solution.py

--- solution.py
+++ solution.py
@@ -13,6 +13,6 @@ def solution(p: float, x: np.array) -> tuple:
     x = 2 * x / 32 ** 2
     alpha = 1 - p
     loc = x.mean()
-    scale = np.sqrt(np.var(x)) / np.sqrt(len(x))
+    scale = 1
     return loc - scale * norm.ppf(1 - alpha / 2), \
            loc - scale * norm.ppf(alpha / 2)
